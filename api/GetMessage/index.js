c177781dfa7c06d8db793a40809df21e8bbaa566
jb <jasontbrown99@gmail.com>
2021-07-29 15:19:08 -0700

    add stuff

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: { text: "Hello from the API" },
-  };
+  context.res.json({
+      text: "Hello from the API"
+  });
 };
